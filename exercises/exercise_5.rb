4633c96996110591447f00521ef0783109f45da1
Princeton Arulnesan <princeton.arul@gmail.com>
2020-06-23 19:06:11 +0000

    finished up exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@storesum = Store.select(:annual_revenue)
+@sum = 0
+@storesum.each do |store|
+  @sum = @sum + store.annual_revenue
+end
+puts @sum
+puts @sum / Store.count
+@big_store = Store.where("annual_revenue >= 1000000")
+puts @big_store.count
